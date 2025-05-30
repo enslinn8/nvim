9c45ff79c78b3ba0a8f58cba1b3f7f8f9d0ce603
Nico Enslin <enslinn8@gmail.com>
2025-05-30 10:26:51 +0200

    theme and lsp key binding updates

--- lua/rustyd2/lazy/theme.lua
+++ lua/rustyd2/lazy/theme.lua
@@ -1,11 +1,7 @@
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('onedark').setup {
-      style = 'darker'
-    }
-    -- Enable theme
-    require('onedark').load()
-  end
+    "folke/tokyonight.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+        vim.cmd [[colorscheme tokyonight-night]]
+    end
 }
--- lua/rustyd2/remap.lua
+++ lua/rustyd2/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
@@ -15,11 +16,27 @@ vim.keymap.set("n", "N", "Nzzzv")
 --past without losing it
 vim.keymap.set("x", "<learder>p", "\"_dP")
 
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+    vim.o.clipboard = 'unnamedplus'
+end)
+
+
 --l    local opts = { buffer = bufnr, remap = false }
 local opts = { buffer = bufnr, remap = false }
 vim.keymap.set('n', 'K', vim.lsp.buf.signature_help, opts)
 vim.keymap.set('n', 'H', vim.lsp.buf.hover, opts)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 vim.keymap.set('n', '<leader>fd', vim.lsp.buf.format, opts)
+vim.keymap.set('n', '<leader>fd', vim.lsp.buf.format, opts)
+vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+
 vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
 vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+
+vim.keymap.set("n", "<leader>ee", function() vim.cmd('NvimTreeToggle') end, opts)
+vim.keymap.set("n", "<leader>ef", function() vim.cmd('NvimTreeFindFile') end, opts)
+vim.keymap.set("n", "<leader>ec", function() vim.cmd('NvimTreeCollapse') end, opts)
